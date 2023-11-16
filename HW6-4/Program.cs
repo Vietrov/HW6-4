071fbad5501a4017f6bc4f729409c6c3ed14db48
PC <PC@DESKTOP-R4P1PJC>
2023-11-16 22:50:04 +0100

    Add project files.

---
+++ HW6-4/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        
+        string[] cards = { "Трефы", "Червы", "Бубны", "Пики" };
+        int cardNumber = 2; 
+
+        DisplayCard(cards, cardNumber);
+    }
+
+    static void DisplayCard(string[] cards, int cardNumber)
+    {
+        
+        if (cardNumber >= 0 && cardNumber < cards.Length)
+        {
+            Console.WriteLine($"Игровая карта под номером {cardNumber + 1}: {cards[cardNumber]}");
+        }
+        else
+        {
+            Console.WriteLine("Карта с указанным номером отсутствует.");
+        }
+    }
+}
